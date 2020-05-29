d034069b762fe839be77db211a0d708afe4b9712
Jo Seong-cheol <josworks27@gmail.com>
2020-05-29 15:19:21 +0900

    Add Image Slider into DressDetailInfo component

--- src/components/dress/DressDetailInfo.js
+++ src/components/dress/DressDetailInfo.js
@@ -1,15 +1,18 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import Coverflow from 'react-coverflow';
 
-const DressDetailInfo = ({ match, images, dress }) => {
+const DressDetailInfo = ({ images, dress }) => {
   return (
     <>
       <div>
         <h3>Dress Info</h3>
-        <div>
-          {images.map((image) => (
-            <img key={image.id} src={image.filePath} alt="" />
-          ))}
-        </div>
+        <Coverflow height={350} displayQuantityOfSide={2} enableHeading={false}>
+          <div role="menuitem" tabIndex="0" />
+          {images.map((img) => {
+            return <img src={img.filePath} alt="" />;
+          })}
+        </Coverflow>
         <div>{dress.model}</div>
         <div>{dress.price}</div>
         <div>{dress['store.name']}</div>
